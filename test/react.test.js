ae65d5aff94647744d2e33ce1df87b4f01769c5d
Martin Wernstål <m4rw3r@gmail.com>
2019-04-02 13:12:39 +0200

    Some work to be able to test packaging

--- test/react.test.js
+++ test/react.test.js
@@ -2,6 +2,4 @@
 import test from "ava";
 import React, { Component, createContext } from "react-dom";
 
-test("basic state", t => {
-  const App = () => <C.Consumer>{s => s}</C.Consumer>;
-})
+test.todo("basic state")
