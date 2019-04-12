06528980b1f0440bf5702c11143c3642f725558f
Martin Wernstål <m4rw3r@gmail.com>
2019-04-12 10:53:22 +0200

    Test: Import parts from react so we get moer accurate code coverage numbers

--- test/react.test.js
+++ test/react.test.js
@@ -1,5 +1,9 @@
 
 import test from "ava";
 import React, { Component, createContext } from "react-dom";
+import { StateContext
+       , useSendMessage
+       , createStateData
+       , useData } from "../react/src/index.js"
 
 test.todo("basic state")
