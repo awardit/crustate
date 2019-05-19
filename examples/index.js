ad09452e6530c1b1b8727f012a3fcf3d9d20f8e0
Martin Wernstål <m4rw3r@gmail.com>
2019-05-19 22:47:38 +0200

    Examples: Removed unnecessary import

--- examples/index.js
+++ examples/index.js
@@ -1,12 +1,11 @@
 /* @flow */
 
 const express = require("express");
-const path    = require("path");
 
 const port = process.env.PORT || 8080;
 const app  = express();
 
 // Static examples
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.listen(port, () => console.log(`Examples are now available on port ${port}.`));
