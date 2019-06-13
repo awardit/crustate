b34524d2a930e005ec889f5edc23562992580784
Martin Wernstål <m4rw3r@gmail.com>
2019-06-13 11:29:56 +0200

    Assert: Rely on dead-code-elimination to remove debugAssert

--- src/assert.js
+++ src/assert.js
@@ -1,7 +1,10 @@
 /* @flow */
 
-export const debugAssert = process.env.NODE_ENV === "production" ? (cond: boolean, msg: string) => {} : (cond: boolean, msg: string) => {
-  if( ! cond) {
+/**
+ * Debug assertion, will be a noop in production builds.
+ */
+export const debugAssert = (cond: boolean, msg: string) => {
+  if(process.env.NODE_ENV !== "production" && ! cond) {
     throw new Error(`Assertion failed: ${msg}`);
   }
 };
