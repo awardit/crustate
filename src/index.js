de39591edc1861f6d145d808f70096c6c6929b4c
Martin Wernstål <m4rw3r@gmail.com>
2019-06-16 13:49:33 +0200

    Format: Align reexports

--- src/index.js
+++ src/index.js
@@ -4,38 +4,26 @@
 //
 // NOTE: Keep in sync with externs.js
 
-export type {
-  Message,
-  MessageTag,
-  MessageFilter,
-  SubscriptionMap,
-  Subscription,
-} from "./message";
-export type {
-  Update,
-  NoUpdate,
-  DataUpdate,
-  MessageUpdate,
-} from "./update";
-export type {
-  Init,
-  StateUpdate,
-  Subscribe,
-  State,
-  StatePath,
-} from "./state";
-export type {
-  Supervisor,
-  Snapshot,
-  StateSnapshot,
-} from "./storage";
+export type { Message
+            , MessageTag
+            , MessageFilter
+            , SubscriptionMap
+            , Subscription } from "./message";
+export type { Update
+            , NoUpdate
+            , DataUpdate
+            , MessageUpdate } from "./update";
+export type { Init
+            , StateUpdate
+            , Subscribe
+            , State
+            , StatePath } from "./state";
+export type { Supervisor
+            , Snapshot
+            , StateSnapshot } from "./storage";
 
-export {
-  NONE,
-  updateData,
-  updateAndSend,
-} from "./update";
-export {
-  StateInstance,
-  Storage,
-} from "./storage";
+export { NONE
+       , updateData
+       , updateAndSend } from "./update";
+export { StateInstance
+       , Storage } from "./storage";
