aa3bf979ec969a6da1c14dc5b66ca6caeea68bd3
Martin Wernstål <m4rw3r@gmail.com>
2019-10-29 17:20:11 +0100

    doc: added note about effect reply being deferred

--- src/effect.js
+++ src/effect.js
@@ -5,6 +5,13 @@ import type { StatePath } from "./storage";
 
 export type Effect<M> = {
   // FIXME: Remove srcPath once async replies is working
+  /**
+   * After registering an `Effect` with a `Storage` `effect` is run when a
+   * message matching `subscribe` is encountered, any returned message will
+   * be sent back to the source state.
+   *
+   * NOTE: The reply is deferred.
+   */
   effect: (msg: M, srcPath: StatePath) => ?Message | Promise<?Message>,
   name?: string,
   subscribe: Subscriptions<M>,
